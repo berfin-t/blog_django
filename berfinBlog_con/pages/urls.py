b98a9662fd8a7e1976d719a51c1a502eb7f9cfe2
berfin-t <tekberfin@outlook.com>
2022-08-03 20:14:31 +0300

    Create contact.html

--- berfinBlog_con/pages/urls.py
+++ berfinBlog_con/pages/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="index"),
+    path('about.html', views.about, name="about"),
+    path('index.html', views.index, name="index"),
+    path('contact.html', views.contact, name="contact"),
+    path('works.html', views.works, name="works"),
 ]
--- berfinBlog_con/pages/views.py
+++ berfinBlog_con/pages/views.py
@@ -3,3 +3,12 @@ from urllib import request
 
 def index(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def works(request):
+    return render(request, 'works.html')
